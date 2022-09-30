253b9f2108b00b39bb4e3b3c5bf3c77e0c444e5c
cBombs <richie.ucsb@gmail.com>
2022-09-29 18:16:06 -0700

    r

--- c++/13-Large-Sum/LargeSum/LargeSum/LargeSum.cpp
+++ c++/13-Large-Sum/LargeSum/LargeSum/LargeSum.cpp
@@ -4,6 +4,8 @@
 
     Project Euler: #13 - Large Sum
 
+    Acknowledgement: Tutorial's Point Sum of Two Large Numbers in C++
+
     Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 */
 
@@ -16,6 +18,11 @@
 
 using namespace std;
 
+string addBigNumber(string s1, string s1) {
+
+    
+}
+
 /*
 
 */
@@ -24,14 +31,20 @@ int main()
     ifstream infile("Number.txt"); // read input from file
     string line;
     vector<string> numbers; // will store file's numbers as strings
+    int sum = 0;
 
     // go through file and push numbers to vector
     while (!inStream.EOF()) {
 
-        getline(infile, lineß);
+        getline(infile, line);
         numbers.push_back(line);
     }
 
-    // manipulate data
+    for (auto num : numbers) {
+
+
+    }
+
+
     // return data
 }
