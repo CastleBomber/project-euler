4380398e7bea86f2be7e966ed528327faee55832
cBombs <richie.ucsb@gmail.com>
2022-10-02 10:02:49 -0700

    testing

--- c++/13-Large-Sum/LargeSum/LargeSum/LargeSum.cpp
+++ c++/13-Large-Sum/LargeSum/LargeSum/LargeSum.cpp
@@ -18,9 +18,42 @@
 
 using namespace std;
 
-string addBigNumber(string s1, string s1) {
+string addBigNumbers(string number1, string number2) {
 
-    
+    if (number1.length() >  number2.length()) {
+
+        swap(number1, number2);
+    }
+
+    string sum = "";
+    int len1 = numer1.length();
+    int len2 = number2.length();
+    int digitDiff = len2 - len1;
+    int carry = 0;
+    int intSum;
+
+    for (int i = len1 - 1; i >= 0; i--) {
+
+        intSum = ((number1[i] - '0') + (number2[i+digitDiff] - '0') + carry);
+        sum.push_back(intSum%10 + '0');
+        carry = intSum/10;
+    }
+
+    for (int i = digitDiff-1; i >= 0; i--) {
+
+        intSum = ((number2[i] - '0') + carry);
+        sum.push_back(intSum%10 + '0');
+        carry = intSum/10;
+    }
+
+    if (carry) {
+
+        sum.push_back(carry + '0');
+    }
+
+    reverse(sum.begin(), sum.end());
+
+    return sum;
 }
 
 /*
@@ -38,13 +71,17 @@ int main()
 
         getline(infile, line);
         numbers.push_back(line);
+        getline(infile, line);
+        numbers.push_back(line);
+
+        addBigNumbers(number[1], numbers[2]);
     }
 
-    for (auto num : numbers) {
+    // for (auto num : numbers) {
+    //
+    //
+    // }
 
 
-    }
-
-
-    // return data
+    return 0;
 }
